a9acc37e052fee7f8a94b95947194a1fd2f72816
Agr-27 <adrianguzmanr27@gmail.com>
2026-08-31 23:49:24 -0600

    Uso de lista y función con ciclos para simular partidos
    Falta:
    -Actualizar la documentación al uso definido de equipos (ya no los agrega la persona con un ciclo for)

--- Proyecto Mundial.py
+++ Proyecto Mundial.py
@@ -1,7 +1,39 @@
-sel = ""
-for i in range(0, 16):
-    cont = input(f"Ingresa la selección {i + 1} de 16: ")
-    sel = sel + f"{i + 1}. {cont}\n"
+import random
+import os
+# Inicio de la lista con los 16 equipos
+sel = ["Brasil", "Francia", "Argentina", "Alemania", "España", "Inglaterra", "Portugal", "Bélgica", "Países Bajos", "Italia", "Croacia", "Uruguay", "México", "Estados Unidos", "Japón", "Marruecos"]
+# Ciclo for para almacenar los 16 equipos
 
+# Se muestra en consola los equipos seleccionados en forma de lista
 print("\nSelecciones Participantes:")
-print(sel)
+for i, equipo in enumerate(sel, start=1):
+    print(f"{i}. {equipo}")
+    
+input("\nPresiona Enter para continuar...")
+print("\n" * 40) #Opcion de limpiar consola para thonny
+#os.system('cls' if os.name == 'nt' else 'clear') No funciona en thonny, solo en terminal
+
+def ronda(sel):
+    ganadores = []
+    for i in range(0, len(sel), 2):
+        e1 = sel[i]
+        e2 = sel[i+1]
+        print(f"Partido: {e1} vs {e2}")
+        gp = int(input("\nIngresa 1 o 2 para escoger al ganador\n"))
+        if gp == 1:
+                 golesg = random.randint(1,5)
+                 golesp = random.randint(0,golesg - 1)   
+                 print(f"\nEl marcador final es: {golesg} - {golesp} Favor {e1}\nPartido: {e1} vs {e2}")
+                 ganadores.append(e1) 
+        elif gp == 2:
+                 golesg = random.randint(1,5)
+                 golesp = random.randint(0,golesg - 1)
+                 print(f"\nEl marcador final es: {golesp} - {golesg} Favor {e2}\nPartido: {e1} vs {e2}")
+                 ganadores.append(e2)
+        input("\nPresiona Enter para continuar...")
+        print("\n" * 40) #Opcion de limpiar consola para thonny
+        #os.system('cls' if os.name == 'nt' else 'clear') No funciona en thonny, solo en terminal  
+                 
+    return ganadores
+ganadores_ronda = ronda(sel)
+print("\nEquipos que pasan a la siguiente ronda:", ganadores_ronda)
