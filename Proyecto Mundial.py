5a0532da39cff7cf84167af8d1e4e255cb69b460
Agr-27 <adrianguzmanr27@gmail.com>
2026-08-31 22:28:07 -0600

    Change input range from 24 to 16 selections

--- Proyecto Mundial.py
+++ Proyecto Mundial.py
@@ -1,6 +1,6 @@
 sel = ""
-for i in range(0, 24):
-    cont = input(f"Ingresa la selección {i + 1} de 24: ")
+for i in range(0, 16):
+    cont = input(f"Ingresa la selección {i + 1} de 16: ")
     sel = sel + f"{i + 1}. {cont}\n"
 
 print("\nSelecciones Participantes:")
