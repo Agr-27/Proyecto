d02128fcc878e6df5d7b4b86011c4ccdf6447ca9
Agr-27 <adrianguzmanr27@gmail.com>
2026-09-09 07:18:01 -0600

    Avance 3

--- Proyecto Mundial.py
+++ Proyecto Mundial.py
@@ -1,46 +1,54 @@
-import random
-import os
-# Se incializa la lista con las 16 selcciones
 sel = ["Brasil", "Francia", "Argentina", "Alemania", "España", "Inglaterra", "Portugal", "Bélgica", "Países Bajos", "Italia", "Croacia", "Uruguay", "México", "Estados Unidos", "Japón", "Marruecos"]
 
+def enumeracion(n,lista): # Funcion que da los equipos en forma de lista
+    print(f"{lista}. {sel[n]}")
 
-# Se muestra en consola los equipos seleccionados en forma de lista
-print("\nSelecciones Participantes:")
-for i, equipo in enumerate(sel, start=1): # Funcion enumerate(forma facil de realizar una lista sin un contador)
-    print(f"{i}. {equipo}")
-    
-input("\nPresiona Enter para continuar...")
-print("\n" * 40) #Opcion de limpiar consola para thonny
-#os.system('cls' if os.name == 'nt' else 'clear') No funciona en thonny, solo en terminal
+# Funcion para la pregunta de cuantos minuts duro el partido de cierta fase
+def pregunta(fase, numero):
+    return int(input(f"\nElige los minutos que quieres que dure el partido de {fase} {numero} (90 o 120): "))
 
-# Se declara la funcion que se usara en las rondas. Ejemplo: ronda(octavos) ronda(cuartos)
-def ronda(sel):
-    # Declaracion de la lista donde se guardaran los ganadores
-    ganadores = []
-    # Ciclo for (inicio, condicion, secuencia) Se empieza del 0 por como la listaes interpretada por python
-    for i in range(0, len(sel), 2): # len permite conocer cuantos datos en la lista
-        e1 = sel[i]
-        e2 = sel[i+1] # Selecciona los enfrentamientos
-        print(f"Partido: {e1} vs {e2}")
-        gp = int(input("\nIngresa 1 o 2 para escoger al ganador\n"))
-        
-        # Condicionales if (victoria, derrota)
-        if gp == 1:
-                 golesg = random.randint(1,5)
-                 golesp = random.randint(0,golesg - 1)  # Funcion random (randint para solo enteros) (minimo, maximo) 
-                 print(f"\nEl marcador final es: {golesg} - {golesp} Favor {e1}\nPartido: {e1} vs {e2}")
-                 ganadores.append(e1) # .append para guardar el equipo en la lista
-        elif gp == 2:
-                 golesg = random.randint(1,5)
-                 golesp = random.randint(0,golesg - 1)
-                 print(f"\nEl marcador final es: {golesp} - {golesg} Favor {e2}\nPartido: {e1} vs {e2}")
-                 ganadores.append(e2)
-            
-        input("\nPresiona Enter para continuar...")
-        print("\n" * 40) #Opcion de limpiar consola para thonny
-        #os.system('cls' if os.name == 'nt' else 'clear') No funciona en thonny, solo en terminal  
+enumeracion(0,1)
+enumeracion(1,2)
+enumeracion(2,3)
+enumeracion(3,4) # Uso de funcion enumeracion
+enumeracion(4,5)
+enumeracion(5,6)
+enumeracion(6,7)
+enumeracion(7,8)
+enumeracion(8,9)
+enumeracion(9,10)
+enumeracion(10,11)
+enumeracion(11,12)
+enumeracion(12,13)
+enumeracion(13,14)
+enumeracion(14,15)
 
-    # Return para sacar la lista ganadores fuera de la funcion
-    return ganadores
-Octavos = ronda(sel) # Se llama la funcion
-print("\nEquipos que pasan a la siguiente ronda:", Octavos) # Se visualiza los equipos ganadores
+# Llamamos la funcion de pregunta
+p1 = pregunta("octavos", 1)
+p2 = pregunta("octavos", 2)
+p3 = pregunta("octavos", 3)
+p4 = pregunta("octavos", 4)
+p5 = pregunta("octavos", 5)
+p6 = pregunta("octavos", 6)
+p7 = pregunta("octavos", 7)
+p8 = pregunta("octavos", 8)
+
+c1 = pregunta("cuartos", 1)
+c2 = pregunta("cuartos", 2)
+c3 = pregunta("cuartos", 3)
+c4 = pregunta("cuartos", 4)
+
+s1 = pregunta("semis", 1)
+s2 = pregunta("semis", 2)
+
+# Operacion de los minutos totales de las fases
+octavos = (p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8) / 8
+cuartos = (c1 + c2 + c3 + c4) / 4
+semis = (s1 + s2) / 2
+
+#  Resultados
+print("\nLa fase de octavos sumando sus partidos dura un total de: ", octavos)
+print("La fase de cuartos sumando sus partidos dura un total de: ", cuartos)
+print("La fase de semis sumando sus partidos dura un total de: ", semis)
+
+# Mejor optimizado si se usara ciclos y condicionales
